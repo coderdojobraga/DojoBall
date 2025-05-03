00fcb8ce8c539fe284a9c0e6cbb57a600af94625
carlos <carloshsf3899@gmail.com>
2025-05-03 19:23:05 +0100

    Add main to server

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+import argparse
 import socketserver
 import pygame
 import pickle
@@ -8,30 +9,74 @@ from threading import Condition, Lock, Thread
 from hot_reloading import hot_cycle
 
 
-pygame.init()  # Ensure pygame is initialized
-
-clock = pygame.time.Clock()
-
-state = State()
-state.ball.x = 400
-state.ball.y = 400
-state_lock = Lock()  # Lock to ensure thread-safe access to shared state
-
-SCREEN_WIDTH = 720
-SCREEN_HEIGHT = 720
-
-last_state_pickle = bytearray()
-clients = []
-inputs = {}
-running = True
+state_lock = Lock()
 send_cond = Condition()
 
+clients = []
+inputs = {}
+last_state_pickle = bytearray()
 
-def game_cycle():
-    global running
-    global last_state_pickle
+state = State()
 
-    hot_cycle(loop.step, state, clock, inputs, state_lock, send_cond, last_state_pickle)
+
+def main():
+    parser = argparse.ArgumentParser(description="Haxball Game Client")
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Enable debug mode with hot reloading.",
+    )
+    parser.add_argument(
+        "-p", "--port", type=int, default=12345, help="Server port (default: 12345)"
+    )
+    args = parser.parse_args()
+
+    debug: bool = args.debug
+    port: int = args.port
+
+    pygame.init()
+
+    clock = pygame.time.Clock()
+
+    state.ball.x = 400
+    state.ball.y = 400
+
+    game_cycle_thread = Thread(
+        target=game_cycle,
+        args=(debug, state, clock, inputs, state_lock, send_cond, last_state_pickle),
+        daemon=True,
+    )
+    game_cycle_thread.start()
+
+    interpreter_thread = Thread(target=interpreter, daemon=True)
+    interpreter_thread.start()
+
+    with socketserver.ThreadingTCPServer(("0.0.0.0", port), GameTCPHandler) as server:
+        print("Server started, waiting for messages...")
+        server.serve_forever()
+
+    game_cycle_thread.join()
+    interpreter_thread.join()
+
+
+def game_cycle(debug, state, clock, inputs, state_lock, send_cond, last_state_pickle):
+    if debug:
+        # Debug mode
+        print("Debug mode enabled. Hot reloading is active.")
+        hot_cycle(
+            loop.step, state, clock, inputs, state_lock, send_cond, last_state_pickle
+        )
+    else:
+        # Normal mode
+        while loop.step(state, clock, inputs, state_lock, send_cond, last_state_pickle):
+            pass
+
+
+def interpreter():
+    while True:
+        line = input(">>> ")
+        exec(line)
 
 
 class GameTCPHandler(socketserver.BaseRequestHandler):
@@ -95,22 +140,5 @@ class GameTCPHandler(socketserver.BaseRequestHandler):
                 del state.players[self.client_address]
 
 
-def interpreter():
-    while True:
-        line = input(">>> ")
-        exec(line)
-
-
 if __name__ == "__main__":
-    game_cycle_thread = Thread(target=game_cycle, daemon=True)
-    game_cycle_thread.start()
-
-    interpreter_thread = Thread(target=interpreter, daemon=True)
-    interpreter_thread.start()
-
-    with socketserver.ThreadingTCPServer(("0.0.0.0", 12345), GameTCPHandler) as server:
-        print("Server started, waiting for messages...")
-        server.serve_forever()
-
-    game_cycle_thread.join()
-    interpreter_thread.join()
+    main()
