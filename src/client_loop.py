a14819ab25b1e2c1d7b0351b8194c6c1039f4aee
CarlosHSF <carloshsf3899@gmail.com>
2025-06-12 03:07:39 +0100

    feat: score and clock hud

--- src/client_loop.py
+++ src/client_loop.py
@@ -79,9 +79,7 @@ def render(state, screen, transparent_surface, name_font, sockname):
     for post in state.posts.values():
         draw_post(screen, post)
 
-    draw_scoreboard(state, screen)
-
-    draw_timer(state.timer, screen)
+    draw_hud(screen, state)
 
     # Atualizar o ecra
     screen.blit(transparent_surface, (0, 0))
@@ -182,3 +180,67 @@ def draw_timer(timer, screen):
     font = pygame.font.SysFont("arial", 30)
     timer_text = font.render(f"{timer // 60:02d}:{timer % 60:02d}", True, pygame.Color("white"))
     screen.blit(timer_text, (540 - timer_text.get_width() // 2, 50))
+
+def draw_hud(screen, state):
+    font = pygame.font.SysFont("Arial Bold", 33)
+
+    border_radius = 5
+    team_rect_size = 23
+    score_width = 35
+    hyphen_width = 6.67
+    colon_width = 6.67
+    hud_height = 35
+    hud_width = 442.73
+    hud_padding = 5
+    clock_digit_width = 15
+
+    hud_pos_x = screen.get_width() / 2 - hud_width / 2
+    hud_pos_y = 0
+    hud_pos_end_x = hud_pos_x + hud_width
+    team_rect_pos_y = (hud_height - team_rect_size) / 2
+    font_pos_y = (hud_height - font.get_height()) / 2 + 2
+
+    hud_background = pygame.Rect(hud_pos_x, hud_pos_y, hud_width, hud_height)
+    pygame.draw.rect(screen, "#1b2025", hud_background, border_bottom_left_radius=border_radius, border_bottom_right_radius=border_radius)
+
+    red_rect_pos = hud_pos_x + hud_padding
+    red_rect = pygame.Rect(red_rect_pos, team_rect_pos_y, team_rect_size, team_rect_size)
+    pygame.draw.rect(screen, COLOR_TEAM_RED, red_rect, border_radius=border_radius)
+
+    score_red = font.render(f"{state.score_red}", True, pygame.Color("white"))
+    score_red_pos = red_rect_pos + team_rect_size
+    screen.blit(score_red, (score_red_pos + (score_width - score_red.get_width()) / 2, font_pos_y))
+
+    score_hyphen = font.render("-", True, pygame.Color("white"))
+    score_hyphen_pos = score_red_pos + score_width + (hyphen_width - score_hyphen.get_width()) / 2
+    screen.blit(score_hyphen, (score_hyphen_pos, font_pos_y))
+
+    score_blue = font.render(f"{state.score_blue}", True, pygame.Color("white"))
+    score_blue_pos = score_red_pos + score_width + hyphen_width
+    screen.blit(score_blue, (score_blue_pos + (score_width - score_blue.get_width()) / 2, font_pos_y))
+
+    blue_rect_pos = score_blue_pos + score_width
+    blue_rect = pygame.Rect(blue_rect_pos, team_rect_pos_y, team_rect_size, team_rect_size)
+    pygame.draw.rect(screen, COLOR_TEAM_BLUE, blue_rect, border_radius=border_radius)
+
+    clock_sec_ones_pos_end = hud_pos_end_x - hud_padding
+
+    clock_sec_ones = font.render(f"{state.timer % 60 % 10}", True, pygame.Color("white"))
+    clock_sec_ones_pos = clock_sec_ones_pos_end - clock_digit_width
+    screen.blit(clock_sec_ones, (clock_sec_ones_pos + (clock_digit_width - clock_sec_ones.get_width()) / 2, font_pos_y))
+
+    clock_sec_tens = font.render(f"{state.timer % 60 // 10}", True, pygame.Color("white"))
+    clock_sec_tens_pos = clock_sec_ones_pos - clock_digit_width
+    screen.blit(clock_sec_tens, (clock_sec_tens_pos + (clock_digit_width - clock_sec_tens.get_width()) / 2, font_pos_y))
+
+    clock_colon = font.render(":", True, pygame.Color("white"))
+    clock_colon_pos = clock_sec_tens_pos - colon_width
+    screen.blit(clock_colon, (clock_colon_pos + (colon_width - clock_colon.get_width()) / 2, font_pos_y))
+
+    clock_min_ones = font.render(f"{state.timer // 60 % 10}", True, pygame.Color("white"))
+    clock_min_ones_pos = clock_colon_pos - clock_digit_width
+    screen.blit(clock_min_ones, (clock_min_ones_pos + (clock_digit_width - clock_min_ones.get_width()) / 2, font_pos_y))
+
+    clock_min_tens = font.render(f"{state.timer // 600}", True, pygame.Color("white"))
+    clock_min_tens_pos = clock_min_ones_pos - clock_digit_width
+    screen.blit(clock_min_tens, (clock_min_tens_pos + (clock_digit_width - clock_min_tens.get_width()) / 2, font_pos_y))
